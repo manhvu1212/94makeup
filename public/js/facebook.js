50357ae1277ccdc204acbb09c5db6aa17fabc15a
vu <vunm@tmh-techlab.vn>
2016-04-21 11:13:58 +0700

    add angular easyfb

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,44 +1,19 @@
-var FACEBOOK = {
-    init: function () {
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: facebookAppId,
-                xfbml: false,
-                status: true,
-                version: facebookAppApi
-            });
-        };
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/vi_VN/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    },
-    share: function () {
-        $('#facebook-share').click(function () {
-            FB.getLoginStatus(function (response) {
-                if (response.status === 'connected') {
-                    console.log('Logged in.');
-                }
-                else {
-                    FB.login(function(response) {
-
-                    }, {
-                        scope: 'email'
-                    });
-                }
-            });
-        });
-    }
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1587610674899891',
+        xfbml: false,
+        status: true,
+        version: 'v2.6'
+    });
 };
 
-$(document).ready(function () {
-    FACEBOOK.init();
-    FACEBOOK.share();
-});
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/vi_VN/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="94makeup">
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/><!-- /Added by HTTrack -->
+<head>
+
+    <meta charset="utf-8">
+    <title>94 Makeup | Modern eCommerce html Template </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="shortcut icon" href="/public/glammy/images/favicon.ico">
+
+    <!-- CSS -->
+    <link href="/public/glammy/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/public/glammy/css/flexslider.css" rel="stylesheet" type="text/css"/>
+    <link href="/public/glammy/css/fancySelect.css" rel="stylesheet" media="screen, projection"/>
+    <link href="/public/glammy/css/animate.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="/public/glammy/css/style.css" rel="stylesheet" type="text/css"/>
+
+    <!-- FONTS -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
+          rel='stylesheet' type='text/css'>
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+    <script src="/public/js/facebook.js"></script>
+</head>
+<body>
+
+<!-- PRELOADER -->
+<div id="preloader"><img src="/public/glammy/images/preloader.gif" alt=""/></div>
+<!-- //PRELOADER -->
+<div class="preloader_hide">
+    <!-- PAGE -->
+    <div id="page">
+
+        @include('layout.header')
+
+
+    </div>
+</div>
+
+<!-- TOVAR MODAL CONTENT -->
+<div id="modal-body" class="clearfix">
+    <div id="tovar_content"></div>
+    <div class="close_block"></div>
+</div><!-- TOVAR MODAL CONTENT -->
+
+<!-- SCRIPTS -->
+<!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if IE]>
+<html class="ie" lang="en"> <![endif]-->
+
+<script src="/public/components/angular/angular.min.js" type="text/javascript"></script>
+<script src="/public/components/angular-route/angular-route.min.js" type="text/javascript"></script>
+<script src="/public/components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="/public/js/app.js"></script>
+
+<script src="/public/glammy/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/public/glammy/js/superfish.min.js" type="text/javascript"></script>
+<script src="/public/glammy/js/jquery.sticky.js" type="text/javascript"></script>
+<script src="/public/glammy/js/parallax.js" type="text/javascript"></script>
+<script src="/public/glammy/js/jquery.flexslider-min.js" type="text/javascript"></script>
+<script src="/public/glammy/js/jquery.jcarousel.js" type="text/javascript"></script>
+<script src="/public/glammy/js/fancySelect.js"></script>
+<script src="/public/glammy/js/animate.js" type="text/javascript"></script>
+<script src="/public/glammy/js/myscript.js" type="text/javascript"></script>
+<script>
+    if (top != self) top.location.replace(self.location.href);
+</script>
+
+</body>
+</html>
